f62e232a7135be30db76a02302d3eb36bd412461
Yoan Ivanov <yoan_ivanov1@yahoo.com>
2026-07-12 18:26:32 +0300

    Set up API docs

--- app/app/settings.py
+++ app/app/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'rest_framework',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +125,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 AUTH_USER_MODEL = 'core.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
