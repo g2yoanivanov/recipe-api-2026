32c48d91fe92e21bd07600f0270d21f517a299a9
Yoan Ivanov <yoan_ivanov1@yahoo.com>
2026-07-15 20:18:13 +0300

    Configured tags endpoints

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -9,6 +9,13 @@ from django.contrib.auth import get_user_model
 from core import models
 
 
+def create_user(email='user@example.com', password='test12345'):
+    """
+    Create and return a new user
+    """
+    return get_user_model().objects.create_user(email, password)
+
+
 class ModelTests(TestCase):
     """
     Test models
@@ -81,3 +88,12 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        """
+        Test creating a tag
+        """
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+
+        self.assertEqual(str(tag), tag.name)
